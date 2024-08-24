421f03ec745b5a5b26ba0a444079d07ea653af76
DaniLEP <danilomanzoli64@gmail..com>
2024-08-24 18:13:29 -0300

    feat: Feita a colocação do Icon, Buttons e line

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,5 +1,11 @@
 import { Button } from "../ui/button";
 
 export default function Footer() {
-  return <div>footer</div>;
+  return (
+    <div>
+      © 2024 VirusHunter. Todos os direitos reservados.
+      <div>Termos de serviço</div>
+      <div>Política de Privacidade</div>
+    </div>
+  );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,18 @@
 import { Button } from "../ui/button";
 
 export default function Header() {
-  return <div>VirusInspector</div>;
+  return (
+    <div className="flex h-[77px] w-full items-center justify-between py-[20px] px-[96px] border-b ">
+      <img src="/icon.svg" />
+      <div className="flex gap-[32px]">
+        {" "}
+        <div>
+          <Button variant="link">Sobre Nós</Button>
+        </div>
+        <div>
+          <Button variant="link">Documentação</Button>
+        </div>
+      </div>
+    </div>
+  );
 }
