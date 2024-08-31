b80ca64ba602de738b61de045c3b56140b8256f3
fernando-c-lima <fernandox.caetano@gmail.com>
2024-08-30 21:24:38 -0300

    style: adiciona bold no texto do botão

--- src/components/dropfile.tsx
+++ src/components/dropfile.tsx
@@ -8,7 +8,7 @@ export default function Upload() {
         <img src="/dowload.svg" alt="download.icon" className="inline-block"/>
         <p className="mt-[10px] text-[18px] ">Adicione o arquivo ou arraste e solte-o nesta área</p>
         <p className="text-[12px] text-custom-gray">Permitido arquivos ZIP, PDF, DOC, PNG E SVG de até 250 MB</p>
-        <Button variant="default" className="text-[16px] mt-[15px] font-semibold">BUSCAR ARQUIVO</Button>
+        <Button variant="default" className="text-[16px] mt-[15px] font-bold">BUSCAR ARQUIVO</Button>
         </div>
 
        
